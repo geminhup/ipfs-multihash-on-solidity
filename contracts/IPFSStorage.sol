102a50ceb1ad8d7d27ae47ac18652f5e8068ea0b
geminhup <geminhup@gmail.com>
2019-02-14 22:22:04 +0900

    Update to 0.4.25 + Updated mapping key
    Changed mapping key : address => email

--- contracts/IPFSStorage.sol
+++ contracts/IPFSStorage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.25;
 
 
 /**
@@ -11,66 +11,52 @@ pragma solidity ^0.4.18;
  * Currently IPFS hash is 34 bytes long with first two segments represented as a single byte (uint8)
  * The digest is 32 bytes long and can be stored using bytes32 efficiently.
  */
+ 
 contract IPFSStorage {
-  struct Multihash {
-    bytes32 digest;
-    uint8 hashFunction;
-    uint8 size;
-  }
+    struct Multihash {
+        bytes32 digest;
+        uint8 hashFunction;
+        uint8 size;
+    }
 
-  mapping (address => Multihash) private entries;
+    mapping (string => Multihash) private entries;
 
-  event EntrySet (
-    address indexed key,
-    bytes32 digest,
-    uint8 hashFunction,
-    uint8 size
-  );
-
-  event EntryDeleted (
-    address indexed key
-  );
-
-  /**
-   * @dev associate a multihash entry with the sender address
-   * @param _digest hash digest produced by hashing content using hash function
-   * @param _hashFunction hashFunction code for the hash function used
-   * @param _size length of the digest
-   */
-  function setEntry(bytes32 _digest, uint8 _hashFunction, uint8 _size)
-  public
-  {
-    Multihash memory entry = Multihash(_digest, _hashFunction, _size);
-    entries[msg.sender] = entry;
-    EntrySet(
-      msg.sender, 
-      _digest, 
-      _hashFunction, 
-      _size
+    event EntrySet (
+        string indexed email,
+        bytes32 digest,
+        uint8 hashFunction,
+        uint8 size
     );
-  }
 
-  /**
-   * @dev deassociate any multihash entry with the sender address
-   */
-  function clearEntry()
-  public
-  {
-    require(entries[msg.sender].digest != 0);
-    delete entries[msg.sender];
-    EntryDeleted(msg.sender);
-  }
+    event EntryDeleted (string indexed email);
 
-  /**
-   * @dev retrieve multihash entry associated with an address
-   * @param _address address used as key
-   */
-  function getEntry(address _address)
-  public
-  view
-  returns(bytes32 digest, uint8 hashfunction, uint8 size)
-  {
-    Multihash storage entry = entries[_address];
-    return (entry.digest, entry.hashFunction, entry.size);
-  }
+    /**
+    * @dev associate a multihash entry with the sender address
+    * @param _digest hash digest produced by hashing content using hash function
+    * @param _hashFunction hashFunction code for the hash function used
+    * @param _size length of the digest
+    */
+    function setEntry(string email, bytes32 _digest, uint8 _hashFunction, uint8 _size) public {
+        Multihash memory entry = Multihash(_digest, _hashFunction, _size);
+        entries[email] = entry;
+        emit EntrySet(email, _digest, _hashFunction, _size);
+    }
+
+    /**
+    * @dev deassociate any multihash entry with the sender address
+    */
+    function clearEntry(string email) public {
+        require(entries[email].digest != 0);
+        delete entries[email];
+        emit EntryDeleted(email);
+    }
+
+    /**
+    * @dev retrieve multihash entry associated with an address
+    * @param _address address used as key
+    */
+    function getEntry(string _email) public view returns(bytes32 digest, uint8 hashfunction, uint8 size) {
+        Multihash storage entry = entries[_email];
+        return (entry.digest, entry.hashFunction, entry.size);
+    }
 }
